7037160617fbf795beca2a83c5b17b7b21310955
Sobrinho7 <felipesobrinhoti@gmail.com>
2023-02-03 14:58:54 -0300

    Adicionar arquivos de projeto.

---
+++ Campeonato-Carioca/PlayMatch.cs
@@ -0,0 +1,27 @@
+﻿namespace Campeonato
+
+{
+    class PlayMatch
+    {
+        public static void playMatch(string teamA, string teamB, Dictionary<string, int> scores)
+        {
+            Console.WriteLine("Jogo entre " + teamA + " e " + teamB);
+            Console.WriteLine("Digite 1 para vitória de " + teamA + ", 2 para vitória de " + teamB + " ou 0 para empate:");
+            int result = Convert.ToInt32(Console.ReadLine());
+            if (result == 1)
+            {
+                scores[teamA] += 3;
+            }
+            else if (result == 2)
+            {
+                scores[teamB] += 3;
+            }
+            else
+            {
+                scores[teamA]++;
+                scores[teamB]++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Campeonato-Carioca/Program.cs
@@ -0,0 +1,50 @@
+﻿using static Campeonato.PlayMatch;
+
+namespace CariocaSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> teams = new List<string> { "Flamengo", "Fluminense", "Vasco", "Botafogo" };
+            Dictionary<string, int> scores = new Dictionary<string, int>();
+
+            // Inicializa as pontuações dos times
+            foreach (string team in teams)
+            {
+                scores[team] = 0;
+            }
+
+            // Primeira rodada
+            Console.WriteLine("Primeira rodada");
+            playMatch(teams[0], teams[1], scores);
+            playMatch(teams[2], teams[3], scores);
+            Console.WriteLine();
+
+            // Segunda rodada
+            Console.WriteLine("Segunda rodada");
+            playMatch(teams[0], teams[2], scores);
+            playMatch(teams[1], teams[3], scores);
+            Console.WriteLine();
+
+            // Terceira rodada
+            Console.WriteLine("Terceira rodada");
+            playMatch(teams[0], teams[3], scores);
+            playMatch(teams[1], teams[2], scores);
+            Console.WriteLine();
+
+            // Ordena a tabela pelo número de pontos
+            scores = scores.OrderByDescending(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+            // Apresenta a tabela final
+            Console.WriteLine();
+            Console.WriteLine("Tabela final:");
+            int position = 1;
+            foreach (KeyValuePair<string, int> entry in scores)
+            {
+                Console.WriteLine(position + "º lugar: " + entry.Key + " com " + entry.Value + " pontos");
+                position++;
+            }
+        }
+    }
+}
